c403511cb0103f415a3f2ae2aa9fa9976fe4bab8
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-09-01 03:21:37 -0300

    feat: Add user ID generation and retrieval functionality

---
+++ app/static/js/modules/user.js
@@ -0,0 +1,19 @@
+const USER_ID_KEY = "inboxFlowUserId";
+
+function generateUUID() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+export function getUserId() {
+  let userId = localStorage.getItem(USER_ID_KEY);
+  if (!userId) {
+    userId = generateUUID();
+    localStorage.setItem(USER_ID_KEY, userId);
+  }
+  return userId;
+}
+
