b46aba4df8b78e6383c15f253e32617335cac303
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-09-01 16:26:36 -0300

    feat: Add DOM elements for text analysis and modal interactions

---
+++ app/static/js/modules/ui/domElements.js
@@ -0,0 +1,48 @@
+export const textAnalysisModal = document.getElementById("text-analysis-modal");
+export const openTextModalBtn = document.getElementById("open-text-modal-btn");
+export const infoModalMessage = document.getElementById("info-modal-message");
+export const confirmationModalTitle = document.getElementById("modal-title");
+export const globalFileInput = document.getElementById("global-file-input");
+export const clearFiltersBtn = document.getElementById("clear-filters-btn");
+export const filterContainer = document.querySelector(".filter-container");
+export const analyzeButtonPage = document.getElementById("analyze-button");
+export const infoModalOkBtn = document.getElementById("info-modal-ok-btn");
+export const fileTypeFilter = document.getElementById("file-type-filter");
+export const modalEmailForm = document.getElementById("modal-email-form");
+export const infoModalTitle = document.getElementById("info-modal-title");
+export const sidebarOverlay = document.getElementById("sidebar-overlay");
+export const menuToggleBtn = document.getElementById("menu-toggle-btn");
+export const uploadFileBtn = document.getElementById("upload-file-btn");
+export const closeModalBtn = document.getElementById("close-modal-btn");
+export const infoModalOverlay = document.getElementById("info-modal");
+export const contentPanel = document.getElementById("content-panel");
+export const historyList = document.getElementById("history-list");
+export const themeToggle = document.getElementById("theme-toggle");
+export const sidebar = document.getElementById("history-sidebar");
+export const emailForm = document.getElementById("email-form");
+export const themeText = document.getElementById("theme-text");
+export const globalLoadingOverlay = document.getElementById(
+  "global-loading-overlay"
+);
+export const zipContainerTemplate = document.getElementById(
+  "zip-container-template"
+);
+export const historyItemTemplate = document.getElementById(
+  "history-item-template"
+);
+export const analyzeButtonModal = document.getElementById(
+  "modal-analyze-button"
+);
+export const historySearchInput = document.getElementById(
+  "history-search-input"
+);
+export const confirmationModalOverlay =
+  document.getElementById("confirmation-modal");
+export const confirmationModalConfirmBtn =
+  document.getElementById("modal-confirm-btn");
+export const confirmationModalCancelBtn =
+  document.getElementById("modal-cancel-btn");
+export const confirmationModalMessage =
+  document.getElementById("modal-message");
+export const body = document.body;
+
